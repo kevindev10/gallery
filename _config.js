1e6e4e0b458af34d9856abd81ba152f1118797bd
kevindev10 <kevindev1@outlook.com>
2025-03-28 12:13:18 +0300

    Changed config.mongoURI

--- _config.js
+++ _config.js
@@ -1,12 +1,23 @@
 var config = {}
 
 // Update to have your correct username and password
+// config.mongoURI = {
+//     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+//     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+//     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+// }
+
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    development: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    test: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 }
 
+
+
+// "mongodb+srv://kevin1345:<db_password>@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 module.exports = config;
