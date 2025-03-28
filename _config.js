2557e3c8853859e600552aff018b4b27f13789c6
kevindev10 <kevindev1@outlook.com>
2025-03-28 12:22:24 +0300

    added the db name to the mongoURI

--- _config.js
+++ _config.js
@@ -7,11 +7,23 @@ var config = {}
 //     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 // }
 
+
+
+
+
 config.mongoURI = {
-    production: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    development: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    test: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    production: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0",
+    development: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster0",
+    test: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster0",
 }
+
+
+
+// config.mongoURI = {
+//     production: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+//     development: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+//     test: "mongodb+srv://kevin1345:kevin1345@cluster0.jowxgba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+// }
 
 
 
